9f0b68a099eba440f116d7d4e14ffb756db33e8a
Thomas Solecki <thomasjsolecki@gmail.com>
2023-12-24 21:43:23 -0500

    Rendering static and dynamic lists examples

--- src/main.rs
+++ src/main.rs
@@ -5,33 +5,64 @@ fn main() {
 }
 #[component]
 fn App() -> impl IntoView {
-    let (count, set_count) = create_signal(0);
-    let doubled_count = move || count() * 2;
-
+    let static_list_of_counters = vec![create_signal(0), create_signal(1), create_signal(2)];
     view! {
         <div style="display:flex; flex-direction:column; justify-content: center; align-items: center;">
-            <button on:click=move |_| {
-                set_count.update(|x| *x += 1)
-            }>
+            <ul>
 
-                "The count is: " {count}
-            </button>
-            <ProgressBar max=50 progress=count/>
-            <ProgressBar max=50 progress=doubled_count/>
+                {static_list_of_counters
+                    .into_iter()
+                    .map(|(count, set_count)| {
+                        return view! {
+                            <li>
+                                <button on:click=move |_| {
+                                    set_count.update(|x| *x += 1)
+                                }>{count}</button>
+                            </li>
+                        }
+                    })
+                    .collect_view()}
+
+            </ul>
+            <DynamicCounterList/>
         </div>
     }
 }
 
 #[component]
-fn ProgressBar<F: Fn() -> i32 + 'static>(
-    #[prop(default = 100)] max: u16,
-    progress: F,
-) -> impl IntoView {
+fn DynamicCounterList() -> impl IntoView {
+    let initial_counters: Vec<_> = vec![(0, create_signal(0))];
+    let mut num_counters = 1;
+    let (counters, set_counters) = create_signal(initial_counters);
     view! {
-        <progress
-            max=max
-            // now this works
-            value=progress
-        ></progress>
+        <button on:click=move |_| {
+            let sig = create_signal(num_counters + 1);
+            set_counters.update(move |counters| { counters.push((num_counters, sig)) });
+            num_counters += 1;
+        }>"Add counter"</button>
+        <ul>
+            <For
+                // the key should be unique and stable for each row
+                // using an index is usually a bad idea, unless your list
+                // can only grow, because moving items around inside the list
+                // means their indices will change and they will all rerender
+                each=counters
+                key=|counter| counter.0
+                children=move |(key, (count, set_count))| {
+                    view! {
+                        <li>
+                            <button on:click=move |_| set_count.update(|x| *x += 1)>{count}</button>
+                            <button on:click=move |_| {
+                                set_counters
+                                    .update(|counters| {
+                                        counters.retain(|(counter_id, _)| counter_id != &key)
+                                    });
+                            }>"Remove"</button>
+                        </li>
+                    }
+                }
+            />
+
+        </ul>
     }
 }
